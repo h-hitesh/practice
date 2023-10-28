e459e18d0fa3bd94eecb4835efe7af052b0c0845
Hitesh Hotchandani <hiteshhotchandani@outlook.com>
2023-10-28 19:23:40 +0530

    Initial Commit

--- src/main/java/leetcode/problems/KthSymbolInGrammar.java
+++ src/main/java/leetcode/problems/KthSymbolInGrammar.java
@@ -8,20 +8,13 @@ public class KthSymbolInGrammar {
     }
 
     public int kthGrammar(int N, int K) {
-
-        // Base case
         if (N == 1 && K == 1) {
             return 0;
         }
-
-        // If K is even, it is in the second half of the previous row
         if (K % 2 == 0) {
-            return (1 - kthGrammar(N-1, K/2));
-        }
-
-        // If K is odd, it is in the first half of the previous row
-        else {
-            return kthGrammar(N-1, (K+1)/2);
+            return (1 - kthGrammar(N - 1, K / 2));
+        } else {
+            return kthGrammar(N - 1, (K + 1) / 2);
         }
     }
 }
